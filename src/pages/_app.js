bc721b6dec2e94860f027c05513253ad74206731
Tuul29 <tuul.tsegmed0329@gmail.com>
2024-08-14 12:18:04 +0800

    loader

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,15 @@
 import "@/styles/globals.css";
 import Layout from "@/components/layout";
+import MyProvider from "@/provider/provider";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <MyProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </MyProvider>
     </>
   );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,13 +3,14 @@ import AllCards from "@/components/all-Post";
 import Hero from "@/components/hero/hero";
 import PostCards from "@/components/trending";
 import { MyContext } from "@/provider/provider";
+import Loader from "@/components/Loader";
 
 export default function Home() {
   const { searchValue, articles, isLoading } = useContext(MyContext);
   return (
     <main className="flex flex-col gap-[100px] p-[100px]">
-      <Hero />
       <h2>Хайлт: {searchValue}</h2>
+      <Hero />
       <div className="flex flex-wrap max-w-[1280px] m-auto gap-4">
         {isLoading ? (
           <Loader />
